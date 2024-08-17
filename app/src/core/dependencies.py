64eb0556aecb13f2d40ef4a736938fe63acf4424
PawelOchal <opawel262@gmail.com>
2024-08-17 22:52:31 +0200

    update auth

--- app/src/core/dependencies.py
+++ app/src/core/dependencies.py
@@ -1,11 +1,15 @@
 from fastapi import Depends, Request, Cookie, HTTPException, status
 from app.src.domain.auth.services import verify_token
-from typing import Optional
+from typing import Optional, Annotated
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/token-openai-cookie")
 
 def get_db(request: Request):
     return request.state.db
 
 def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
     access_token: Optional[str] = Cookie(None)) -> str:
     """
     Get the current user by using the access token from cookies.
--- app/src/routers/auth.py
+++ app/src/routers/auth.py
@@ -1,26 +1,37 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Cookie, Response
 from sqlalchemy.orm import Session
-from app.src.domain.auth import schemas
+from app.src.domain.auth.schemas import Token, CreateToken, AccessToken
 from app.src.domain.auth.services import get_tokens, get_access_token_by_refresh_token
+from app.src.domain.auth.schemas import Token
 from app.src.core.security import ACCESS_TOKEN_EXPIRE_MINUTES, REFRESH_TOKEN_EXPIRE_DAYS
 from app.src.core.dependencies import get_db
-from typing import Union, Optional
+from typing import Union, Optional, Annotated
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 router = APIRouter(
     prefix='/auth',
     tags=['Auth'],
 )
 
-@router.post('/token', response_model=schemas.Token, status_code=status.HTTP_201_CREATED)
-async def authenticate_user(data: schemas.CreateToken, response: Response, db: Session = Depends(get_db)):
+@router.post('/token-cookie', response_model=Token, status_code=status.HTTP_201_CREATED)
+async def authenticate_user(response: Response, data: CreateToken, db: Session = Depends(get_db)) -> Token:
     tokens = await get_tokens(data=data, db=db)
     response.set_cookie(key='access_token', value=tokens.get('access_token'), httponly=True, max_age=int(ACCESS_TOKEN_EXPIRE_MINUTES.total_seconds()))
     response.set_cookie(key='refresh_token', value=tokens.get('refresh_token'), httponly=True, max_age=int(REFRESH_TOKEN_EXPIRE_DAYS.total_seconds()))
-    return tokens
-@router.post('/refresh-token', response_model=schemas.AccessToken, status_code=status.HTTP_201_CREATED)
+    return Token(**tokens)
+
+@router.post('/token-openai-cookie', response_model=Token, status_code=status.HTTP_201_CREATED)
+async def authenticate_user_form(response: Response,form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)) -> Token:
+    if form_data:
+        data = CreateToken(email=form_data.username, password=form_data.password)
+    tokens = await get_tokens(data=data, db=db)
+    response.set_cookie(key='access_token', value=tokens.get('access_token'), httponly=True, max_age=int(ACCESS_TOKEN_EXPIRE_MINUTES.total_seconds()))
+    response.set_cookie(key='refresh_token', value=tokens.get('refresh_token'), httponly=True, max_age=int(REFRESH_TOKEN_EXPIRE_DAYS.total_seconds()))
+    return Token(**tokens)
+
+
+@router.post('/refresh-token-cookie', response_model=AccessToken, status_code=status.HTTP_201_CREATED)
 async def refresh_token(response: Response, refresh_token: Optional[str] = Cookie(None)):
     access_token = await get_access_token_by_refresh_token(refresh_token)
     response.set_cookie(key='access_token', value=access_token, httponly=True, max_age=int(ACCESS_TOKEN_EXPIRE_MINUTES.total_seconds()))
